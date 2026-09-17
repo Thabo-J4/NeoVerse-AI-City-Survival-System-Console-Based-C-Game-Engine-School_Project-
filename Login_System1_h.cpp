7cbe2a84d4b94e08072d791afca11d31ee72804b
Thabo <thabomodisadife30@gmail.com>
2026-09-17 15:25:14 +0200

    Added Login_Systems1_h.cpp

---
+++ Login_System1_h.cpp
@@ -0,0 +1,28 @@
+#ifndef LOGINSYSTEM_H
+#define LOGINSYSTEM_H
+
+#include <vector>
+#include <string>
+#include "Engineer_h.cpp"
+
+class LoginSystem
+{
+private:
+    vector<Engineer> engineers;
+
+public:
+    void addEngineer(const Engineer &e);
+    vector<Engineer> &getEngineers();
+
+    int sequentialSearchByUsername(const string &username) const;
+
+    int binarySearchByID(const string &id) const;
+
+    void sortByID();
+
+    // Wrappers combining search + password check
+    Engineer *loginLinear(const string &username, const string &password);
+    Engineer *loginBinary(const string &id, const string &password);
+};
+
+#endif
